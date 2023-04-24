49fd379baa55067e3701411ba469166f7e03034e
Franck <gwadafrank@gmail.com>
2023-04-24 18:59:22 +0300

    implement search value

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -5,21 +5,26 @@ import Item from "./Item";
 
 function ShoppingList({ items }) {
   const [selectedCategory, setSelectedCategory] = useState("All");
+  const [searchTerm, setSearchTerm ] = useState('')
 
   function handleCategoryChange(event) {
     setSelectedCategory(event.target.value);
   }
-
+  function handleSearchChange(event){
+    setSearchTerm(event.target.value)
+  }
   const itemsToDisplay = items.filter((item) => {
-    if (selectedCategory === "All") return true;
-
+    // console.log('serc', searchTerm);
+    if(searchTerm) {console.log('serc', searchTerm); return item.name.toLowerCase().includes(searchTerm)}
+    else if (selectedCategory === "All") return true;
     return item.category === selectedCategory;
   });
 
+  // console.log(itemsToDisplay);
   return (
     <div className="ShoppingList">
       <ItemForm />
-      <Filter onCategoryChange={handleCategoryChange} />
+      <Filter onCategoryChange={handleCategoryChange} onSearchChange={handleSearchChange} searchTerm={searchTerm}/>
       <ul className="Items">
         {itemsToDisplay.map((item) => (
           <Item key={item.id} name={item.name} category={item.category} />
